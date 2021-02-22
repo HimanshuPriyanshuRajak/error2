6aa5be6afb4338056cc8364d4c8a3432dfe8340a
HimanshuPriyanshuRajak <74544371+HimanshuPriyanshuRajak@users.noreply.github.com>
2021-02-22 12:42:55 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -1,13 +1,13 @@
 class Box{
-    constructor(x, y, width, height) {
+    constructor(x, y) {
       var options = {
           'restitution':0.8,
           'friction':0.3,
           'density':1.0
       }
-      this.body = Bodies.rectangle(x, y, width, height, options);
-      this.width = width;
-      this.height = height;
+      this.body = Bodies.rectangle(x, y, 20, 50, options);
+      this.width = 20;
+      this.height = 50;
       
       World.add(World, this.body);
     }
